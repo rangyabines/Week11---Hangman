30155e08e9bfc66487a262275b2f7990df44f926
Rangy Abines <rangyabines11@gmail.com>
2016-07-17 21:01:52 -0400

    added file

---
+++ letter.js
@@ -0,0 +1,15 @@
+function splitWords(){
+	this.word = word.split('');
+	for(var i = 0; i < word.lenght; i++){
+		this.word[i] = "_ ";
+	}
+	console.log(word[i]);
+}
+
+
+
+
+
+
+
+module.exports = splitWords;
